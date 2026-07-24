526f3bbaf0d70af7431ef88821350e0456c2a805
libresoul <contact@damindu.me>
2026-07-24 14:04:01 +0530

    chore: remove unnecessary log middleware

--- main.go
+++ main.go
@@ -7,13 +7,6 @@ import (
 	"sync/atomic"
 )
 
-func _(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
-	})
-}
-
 type apiConfig struct {
 	fileServerHits atomic.Int32
 }
