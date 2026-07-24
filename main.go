f4002d5d5aa98677b71bc277729fce1dc4b84ebd
libresoul <contact@damindu.me>
2026-07-24 12:07:46 +0530

    feat: add rediness endpoint, moves fileserver handler to /app

--- main.go
+++ main.go
@@ -7,9 +7,23 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerRediness)
 
 	server := &http.Server{Addr: ":8080", Handler: mux}
 	log.Println("Server started on port 8080")
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerRediness(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		log.Fatal("Failed to write response: ", err)
+	}
 }
